73873451b521fe9e7128ee648413f6f2ac956562
ele <ele.enkova@gmail.com>
2025-07-11 13:52:41 +0300

    final

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
+	"os"
+	"yp-go6/internal/server"
+)
 
+func main() {
+	logger := log.New(os.Stdout, "INFO: ", log.Ldate|log.Ltime)
+
+	server := server.Create(logger)
+	port := ":" + server.Server.Addr
+
+	err := http.ListenAndServe(port, server.Server.Handler)
+	if err != nil {
+		server.Logger.Fatal(err)
+	}
 }
--- internal/server/server.go
+++ internal/server/server.go
@@ -1,1 +1,34 @@
 package server
+
+import (
+	"log"
+	"net/http"
+	"time"
+	"yp-go6/internal/handlers"
+)
+
+type Server struct {
+	Logger *log.Logger
+	Server *http.Server
+}
+
+func Create(logger *log.Logger) Server {
+
+	router := http.NewServeMux()
+	router.HandleFunc("/", handlers.GetHtml)
+	router.HandleFunc("/upload", handlers.Upload)
+
+	server := http.Server{
+		Addr:         "8080",
+		Handler:      router,
+		ErrorLog:     logger,
+		ReadTimeout:  time.Second * 5,
+		WriteTimeout: time.Second * 10,
+		IdleTimeout:  time.Second * 15,
+	}
+
+	return Server{
+		Logger: logger,
+		Server: &server,
+	}
+}
